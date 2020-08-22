f5a12c5c899e64f076e955d8d561d2e3d29dc0d2
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-08-22 22:20:36 +0700

    restructure: the project structure

---
+++ ds01_pair_sum/ds01_pair_sum.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cstdio>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, M, T;
+    scanf("%d%d", &N, &M);
+    vector<int> A;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &T);
+        A.push_back(T);
+    }
+
+    sort(A.begin(), A.end());
+
+    for (int i = 0; i < M; i++) {
+        scanf("%d", &T);
+        bool b = false;
+        for (int j = 0; j < N; j++) {
+            if (binary_search(A.begin() + j + 1, A.end(), T - A[j])) {
+                b = true;
+                break;
+            }
+        }
+        printf("%s\n", b ? "YES" : "NO");
+    }
+}
